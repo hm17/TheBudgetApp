d51bfe94d97f9c73a7301921ede91c8648b62f77
HM <hello@hazella.co>
2019-12-25 11:18:44 -0600

    Initial build

---
+++ app/src/main/java/com/hm/thebudgetapp/AddBudgetActivity.java
@@ -0,0 +1,13 @@
+package com.hm.thebudgetapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AddBudgetActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_budget);
+    }
+}
