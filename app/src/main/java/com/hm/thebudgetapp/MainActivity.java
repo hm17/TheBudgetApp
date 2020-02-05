ba668bfff88acf8c293b7026fe4792261c83e04a
HM <hello@hazella.co>
2020-02-04 20:15:27 -0600

    Added static data to edit layouts

--- app/src/main/java/com/hm/thebudgetapp/MainActivity.java
+++ app/src/main/java/com/hm/thebudgetapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -15,6 +16,14 @@ public class MainActivity extends Activity {
 
     public void createNewBudget(View view) {
         Intent intent = new Intent(this, AddBudgetActivity.class);
+        startActivity(intent);
+    }
+
+    public void launchBudget(View view) {
+        Toast msg = Toast.makeText(this, "YO!", Toast.LENGTH_SHORT);
+        msg.show();
+
+        Intent intent = new Intent(this, ViewBudgetActivity.class);
         startActivity(intent);
     }
 }
