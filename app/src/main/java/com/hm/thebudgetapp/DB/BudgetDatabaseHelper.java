7bc95b9ff39b069710ffd97bd0d403d0faf0b6db
HM <hello@hazella.co>
2020-02-25 12:59:39 -0600

    Added DB functionality

--- app/src/main/java/com/hm/thebudgetapp/DB/BudgetDatabaseHelper.java
+++ app/src/main/java/com/hm/thebudgetapp/DB/BudgetDatabaseHelper.java
@@ -1,22 +1,73 @@
 package com.hm.thebudgetapp.DB;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class BudgetDatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "budgetapp";
+    private static final int DB_VERSION = 3;
+
     public BudgetDatabaseHelper (Context context) {
 
+        super(context, DB_NAME, null, DB_VERSION);
+        //super(context, null, null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        updateDB(db, 0, DB_VERSION);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateDB(db, oldVersion, newVersion);
+    }
 
+    private void updateDB(SQLiteDatabase db, int oldVersion, int newVersion) {
+        if(oldVersion < 1) {
+            db.execSQL("CREATE TABLE BUDGET ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "NAME TEXT, "
+                    + "CATEGORY TEXT, "
+                    + "BALANCE TEXT);");
+
+        }
+
+        if(oldVersion <2) {
+            db.execSQL("CREATE TABLE BUDGET_TRANSACTION ("
+                    + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                    + "budgetId INTEGER, "
+                    + "VENDOR TEXT, "
+                    + "DESCRIPTION TEXT, "
+                    + "AMOUNT TEXT);");
+        }
+
+        if(oldVersion <4) {
+            insertBudget(db, "Food", "food", String.valueOf(150.00));
+            insertBudget(db, "Inventory", "merch", String.valueOf(12));
+            insertBudget(db, "Gas", "misc", String.valueOf(0));
+            insertTransaction(db, 1, "Wawa", "gas", String.valueOf(47.88));
+        }
+
+    }
+
+    private static void insertBudget(SQLiteDatabase db, String name, String category, String balance) {
+        ContentValues budgetValues = new ContentValues();
+        budgetValues.put("NAME", name);
+        budgetValues.put("CATEGORY", category);
+        budgetValues.put("BALANCE", balance);
+        db.insert("BUDGET", null, budgetValues);
+    }
+
+    private static void insertTransaction(SQLiteDatabase db, int budgetId, String vendor, String description, String amount) {
+        ContentValues transactionValues = new ContentValues();
+        transactionValues.put("budgetId", budgetId);
+        transactionValues.put("VENDOR", vendor);
+        transactionValues.put("DESCRIPTION", description);
+        transactionValues.put("AMOUNT", amount);
+        db.insert("BUDGET_TRANSACTION", null, transactionValues);
     }
 }
--- app/src/main/java/com/hm/thebudgetapp/TopFragment.java
+++ app/src/main/java/com/hm/thebudgetapp/TopFragment.java
@@ -1,13 +1,21 @@
 package com.hm.thebudgetapp;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.hm.thebudgetapp.DB.BudgetDatabaseHelper;
 
 
 /**
@@ -23,12 +31,48 @@ public class TopFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(R.layout.fragment_top, container, false);
+
+        Budget testBudget = new Budget("", "", 0);
+
+        // Create a cursor
+        SQLiteOpenHelper budgetDatabaseHelper = new BudgetDatabaseHelper(getActivity());
+        try {
+            SQLiteDatabase db = budgetDatabaseHelper.getReadableDatabase();
+            Cursor cursor = db.query("BUDGET",
+                                        new String[] {"_id", "NAME", "CATEGORY", "BALANCE"},
+                                        null, null, null, null, null);
+            Log.v("TopFragment", "cursor: " + cursor.getCount());
+
+            // Move to first record in the Cursor
+            if (cursor.moveToFirst()) {
+                Log.v("TopFragment", "cursor: " + cursor.getColumnName(0));
+                Log.v("TopFragment", "cursor: " + cursor.toString());
+
+                // Get details from cursor
+                String nameText = cursor.getString(1);
+                Log.v("TopFragment", "nameText: " + nameText);
+                String categoryText = cursor.getString(2);
+                Log.v("TopFragment", "categoryText: " + categoryText);
+                Double balanceValue = Double.valueOf(cursor.getString(3));
+                Log.v("TopFragment", "balanceValue: " + balanceValue);
+                testBudget = new Budget(nameText, categoryText, balanceValue);
+            }
+            cursor.close();
+            db.close();
+        } catch(SQLiteException e) {
+            Log.v("TopFragment", "error: " + e.getMessage());
+            Toast toast = Toast.makeText(getActivity(), "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+
 
         Budget[] budgets = {new Budget("FOOD", "food", 0), new Budget("SHOPPING", "misc", 65.30),
                 new Budget("FOOD", "food", 0), new Budget("SHOPPING", "misc", 65.30),
                 new Budget("FOOD", "food", 0), new Budget("SHOPPING", "misc", 65.30),
-                new Budget("FOOD", "food", 0), new Budget("SHOPPING", "misc", 65.30)};
+                new Budget("FOOD", "food", 0), new Budget("SHOPPING", "misc", 65.30),
+                testBudget};
 
         BudgetAdapter adapter = new BudgetAdapter(budgets);
         recyclerView.setAdapter(adapter);
